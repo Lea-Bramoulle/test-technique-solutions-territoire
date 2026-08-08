bedcc1ee5fceb51914bfa8f1d88a3321b77017a0
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2026-08-08 16:54:34 +0200

    :rocket: feat : add format condition using negative lookahead

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -3,5 +3,5 @@ class Parcel
     # prefix optional with 3 digits
     # section with 1 or 2 uppercase letters or 2 digits
     # plan with 4 digits (mandatory if section is composed of digits) or 1-4 digits
-    NUMBER_REGEXP = /\A(?:(?<prefix>\d{3})[ ])?(?<section>[A-Z]{1,2}|\d{2})[ ](?<plan>\d{4}|\d{1,4})\z/
+    NUMBER_REGEXP = /\A(?:(?<prefix>\d{3})[ ])?(?!\d{2}[ ]\d{1,3}\z)(?<section>[A-Z]{1,2}|\d{2})[ ](?<plan>\d{1,4})\z/
 end

efb915d9941edfa6b80f7960c2fc55c729bcaccd
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2026-08-11 19:19:02 +0200

    :rocket: feat : implement update Commune methode by code_insee with manadatory name value and return 204 no content

--- app/controllers/communes_controller.rb
+++ app/controllers/communes_controller.rb
@@ -3,6 +3,7 @@ require "csv"
 class CommunesController < ApplicationController
   rescue_from ActionController::UnknownFormat, with: -> { head :not_acceptable }
   rescue_from ActiveRecord::RecordNotFound, with: -> { head :not_found }
+  rescue_from ActionController::ParameterMissing, with: -> { head :bad_request }
 
   def index
     @communes = Commune.all
@@ -23,6 +24,15 @@ class CommunesController < ApplicationController
   end
 
   def update
+    commune = Commune.find_by!(code_insee: params[:id])
+    commune.update!(commune_model_params)
+    head :no_content
+  end
+
+  private
+
+  def commune_model_params
+    params.require(:commune).permit(:name)
   end
 
 end
